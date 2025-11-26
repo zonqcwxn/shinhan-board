4c70499e5a8e252c598a5cbaf7efe067f54ddbee
minsjes <kangminje00@gmail.com>
2025-11-26 12:06:39 +0900

    DAO, DTO

---
+++ src/com/shinhan/dao/DAO.java
@@ -0,0 +1,5 @@
+package com.shinhan.dao;
+
+public class DAO {
+
+}
---
+++ src/com/shinhan/dto/DTO.java
@@ -0,0 +1,22 @@
+package com.shinhan.dto;
+
+import java.sql.Date;
+
+public class DTO {
+
+	Integer USER_ID;
+    String USER_NAME;
+    String USER_PWD;
+    String USER_EMAIL;
+    Date USER_REGDATE;
+    String USER_ACTIVE;
+
+    Integer POST_ID;
+    String POST_TITLE;
+    String POST_CONTENT;
+    Date POST_REGDATE;
+    Date POST_UDTDATE;
+    Integer POST_VIEW;
+    String POST_ACTIVE;
+    Integer POST_USER_ID;
+}
