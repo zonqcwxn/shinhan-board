a23e7f2242bc6398df4488e7ac6773878c55809d
GiJeong <rlwjd0025@gmail.com>
2025-11-27 09:07:09 +0900

    DAO, Service 동작

--- src/com/shinhan/dao/DAO.java
+++ src/com/shinhan/dao/DAO.java
@@ -11,19 +11,7 @@ import com.shinhan.dto.BoardDTO;
 import com.shinhan.util.DBUtil;
 
 public class DAO {
-	// 유저 테이블 전체 조회
-	/*
-	 * public static List<DTO> userselectAll() { Connection conn = null;
-	 * PreparedStatement st = null; ResultSet rs = null; String sql =
-	 * "select * from tbl_user"; List<DTO> dtolist = new ArrayList<DTO>(); try {
-	 * conn = DBUtil.dbConnect(); st = conn.prepareStatement(sql); rs =
-	 * st.executeQuery(); while (rs.next()) { DTO dto = makeuser(rs);
-	 * dtolist.add(dto); } } catch (SQLException e) { // TODO Auto-generated catch
-	 * block e.printStackTrace(); } finally { DBUtil.dbDisConnect(conn, st, rs); }
-	 * 
-	 * return dtolist; }
-	 */
-	//보드 테이블 전체 조회
+	// 蹂대�� ���대� ��泥� 議고��
 	public static List<BoardDTO> boardselectAll() {
 		Connection conn = null;
 		PreparedStatement st = null;
@@ -47,25 +35,14 @@ public class DAO {
 
 		return dtolist;
 	}
-	//유저 ID로 조회
-	/*
-	 * public static DTO selectuserId(int uId) { Connection conn = null;
-	 * PreparedStatement st = null; ResultSet rs = null; String sql =
-	 * "select * from tbl_user where user_id = ?"; DTO dto = null; try { conn =
-	 * DBUtil.dbConnect(); st = conn.prepareStatement(sql); st.setInt(1, uId); rs =
-	 * st.executeQuery(); if (rs.next()) { dto = makeuser(rs); } } catch
-	 * (SQLException e) { // TODO Auto-generated catch block e.printStackTrace(); }
-	 * finally { DBUtil.dbDisConnect(conn, st, rs); } return dto;
-	 * 
-	 * }
-	 */
-	//보드 ID로 조회
+
+	// 蹂대�� ID濡� 議고��
 	public static BoardDTO selectboardId(int bId) {
 		Connection conn = null;
 		PreparedStatement st = null;
 		ResultSet rs = null;
 		String sql = """
-				select * from tbl_board 
+				select * from tbl_board
 				where board_user_id = ? and board_active = 'Y'
 				""";
 		BoardDTO dto = null;
@@ -74,7 +51,7 @@ public class DAO {
 			st = conn.prepareStatement(sql);
 			st.setInt(1, bId);
 			rs = st.executeQuery();
-			if(rs.next()) {
+			if (rs.next()) {
 				dto = makeboard(rs);
 			}
 		} catch (SQLException e) {
@@ -84,86 +61,63 @@ public class DAO {
 			DBUtil.dbDisConnect(conn, st, rs);
 		}
 		return dto;
-		
+
 	}
-	/*
-	 * public static int insertuser(DTO dto) { Connection conn = null;
-	 * PreparedStatement st = null; String sql =
-	 * "insert into tbl_user(user_id, user_name, user_pwd, user_email, user_regdate, user_active) values(seq_user_id.NEXTVAL, ?, ?, ?, SYSDATE, 'Y' )"
-	 * ; int result = 0;
-	 * 
-	 * try { conn = DBUtil.dbConnect(); st = conn.prepareStatement(sql);
-	 * st.setString(1, dto.getUser_name()); st.setString(2, dto.getUser_pwd());
-	 * st.setString(3, dto.getUser_email()); result = st.executeUpdate(); } catch
-	 * (SQLException e) { // TODO Auto-generated catch block e.printStackTrace();
-	 * }finally { DBUtil.dbDisConnect(conn, st, null); } return result; }
-	 */
-	
+
 	public static int insertboard(BoardDTO dto) {
 		Connection conn = null;
 		PreparedStatement st = null;
-		String sql = "insert into tbl_board(board_id, board_title, board_content, board_active, board_view) "
-				+ "values(board_seq.NEXTVAL, ?, ?, 'Y', 0";
+		String sql = """
+				INSERT INTO tbl_board
+				(board_id, board_title, board_content, board_active, board_view, board_regdate, board_udtdate, board_user_id)
+				VALUES (board_seq.NEXTVAL, ?, ?, 'Y', 0, SYSDATE, SYSDATE, ?)
+				""";
+
 		int result = 0;
-		
-		
+
 		try {
 			conn = DBUtil.dbConnect();
 			st = conn.prepareStatement(sql);
 			st.setString(1, dto.getBoard_title());
 			st.setString(2, dto.getBoard_content());
-			
+			st.setInt(3, dto.getBoard_user_id());
 			result = st.executeUpdate();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}finally {
+		} finally {
 			DBUtil.dbDisConnect(conn, st, null);
 		}
 		return result;
 	}
-	
+
 	public static int updateboard(BoardDTO dto) {
 		Connection conn = null;
 		PreparedStatement st = null;
-		String sql = "update tbl_board set board_title = ?, board_content = ?, board_udtdate = SYSDATE from tbl_board where = ?";
+		String sql = "update tbl_board set board_title = ?, board_content = ?, board_udtdate = SYSDATE where board_id = ?";
 		int result = 0;
-		
-		
+
 		try {
 			conn = DBUtil.dbConnect();
 			st = conn.prepareStatement(sql);
 			st.setString(1, dto.getBoard_title());
 			st.setString(2, dto.getBoard_content());
-			
+			st.setInt(3, dto.getBoard_user_id());
+
 			result = st.executeUpdate();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}finally {
+		} finally {
 			DBUtil.dbDisConnect(conn, st, null);
 		}
 		return result;
 	}
-	/*
-	 * public static int deleteuser(int user_id) { Connection conn = null;
-	 * PreparedStatement st = null; String sql =
-	 * "update tbl_user set user_acitve = 'N' from tbl_user where user_id = ? and user_active = 'Y'"
-	 * ; int result = 0;
-	 * 
-	 * try { conn = DBUtil.dbConnect(); st = conn.prepareStatement(sql);
-	 * st.setInt(1, user_id); result = st.executeUpdate();
-	 * 
-	 * } catch (SQLException e) { e.printStackTrace(); }finally {
-	 * DBUtil.dbDisConnect(conn, st, null); }
-	 * 
-	 * return result; }
-	 */
 
 	public static int deleteboard(int board_id) {
 		Connection conn = null;
 		PreparedStatement st = null;
-		String sql = "update tbl_board set board_active = 'N' from tbl_board where board_id = ? and board_active = 'Y'";
+		String sql = "update tbl_board set board_active = 'N', board_udtdate=SYSDATE  where board_id = ? and board_active = 'Y'";
 		int result = 0;
 
 		try {
@@ -174,22 +128,12 @@ public class DAO {
 
 		} catch (SQLException e) {
 			e.printStackTrace();
-		}finally {
+		} finally {
 			DBUtil.dbDisConnect(conn, st, null);
 		}
 
 		return result;
 	}
-
-	/*
-	 * private static DTO makeuser(ResultSet rs) throws SQLException { DTO dto = new
-	 * DTO(); dto.setUser_id(rs.getInt("user_id"));
-	 * dto.setUser_name(rs.getString("user_name"));
-	 * dto.setUser_pwd(rs.getString("user_pwd"));
-	 * dto.setUser_email(rs.getString("user_email"));
-	 * dto.setUser_regdate(rs.getDate("user_regdate"));
-	 * dto.setUser_active(rs.getString("user_active")); return dto; }
-	 */
 
 	private static BoardDTO makeboard(ResultSet rs) throws SQLException {
 		BoardDTO dto = new BoardDTO();
--- src/com/shinhan/service/Service.java
+++ src/com/shinhan/service/Service.java
@@ -6,18 +6,18 @@ import com.shinhan.dao.DAO;
 import com.shinhan.dto.BoardDTO;
 
 public class Service {
-	/*
-	 * public static List<DTO> userselectAll(){ return DAO.userselectAll(); }
-	 */
 	public static List<BoardDTO> boardselectAll(){
 		return DAO.boardselectAll();
 	}
 
-	/*
-	 * public static int deleteuser(int user_id) { return DAO.deleteuser(user_id); }
-	 */
+	public static BoardDTO selectboardId(int board_id){
+		return DAO.selectboardId(board_id);
+	}
 	public static int deleteboard(int board_id) {
 	    return DAO.deleteboard(board_id);
 	}
 	
+	public static int updateboard(BoardDTO board_id) {
+		return DAO.updateboard(board_id);
+	}
 }
